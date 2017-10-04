2781f62e311a60dfb114c4d606a8d2a69017342b
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-04 13:30:21 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,11 @@
-class Vehicle #parent class 
+class Vehicle #parent class
   attr_accessor :wheel_size, :wheel_number
 
   def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
     @wheel_number = wheel_number
   end
-  def go 
+  def go
    "vrrrrrrrooom!"
   end
 
